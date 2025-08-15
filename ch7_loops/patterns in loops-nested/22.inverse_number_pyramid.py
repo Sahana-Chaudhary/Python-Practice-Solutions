610d5c32d411ba885cdef9e3fcab32fcf76a81f7
Sahana Chaudhary <89292729+Sahana-Chaudhary@users.noreply.github.com>
2025-08-15 14:36:59 +0530

    python

---
+++ ch7_loops/patterns in loops-nested/22.inverse_number_pyramid.py
@@ -0,0 +1,18 @@
+'''
+print pattern
+1 2 3 4 5 4 3 2 1
+  1 2 3 4 3 2 1
+    1 2 3 2 1
+      1 2 1
+        1
+'''
+
+r=int(input("Enter the number: "))
+for i in range(r,0,-1):
+    for space in range (r-i):
+        print(" ",end=" ")
+    for num in range(1,i+1):
+        print(num,end=" ")
+    for num in range(i-1,0,-1):
+        print(num,end=" ")
+    print()
